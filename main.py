04cea74ebf1c73666d9b7efa57d66b3aeb23d66c
zhan0903 <z.han0903@gmail.com>
2020-01-28 23:09:53 +1100

    debug

--- main.py
+++ main.py
@@ -373,9 +373,9 @@ if __name__ == '__main__':
 
         if timesteps % 15 == 0:
             print("elite_fitness",elite_fitness)
-            # print("function_target, mean, std",function_target.mean,function_target.std)
-            # mean,std = ray_get_and_free(engine.get_mean_std.remote())
-            # print("function_network, mean, std",mean,std)
+            print("function_target, mean, std",function_target.mean,function_target.std)
+            mean,std = ray_get_and_free(engine.get_mean_std.remote())
+            print("function_network, mean, std",mean,std)
             # print("fitness",fitness)
 
 
